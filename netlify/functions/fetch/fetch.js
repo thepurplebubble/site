19ca5ff8b205e6f960e2e3d02430c87c774f3cca
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2023-12-17 20:13:44 -0500

    feat: switched to axios for request

--- netlify/functions/fetch/fetch.js
+++ netlify/functions/fetch/fetch.js
@@ -1,34 +1,37 @@
 // fetch.js
+const axios = require('axios');
 
-exports.handler = async function (event, context) {
+exports.handler = async function (event, context) {  
   try {
     // Parse the incoming form data from the request body
-    const body = JSON.parse(event.body);
-    // serialise the JSON object to a string in the format ame=kieran&email=test%40test.com&gdpr=consent+given&hp=&list=xjAecLBfJ3HdnmuBXhvkfg&subform=yes&boolean=true&submit=
-    const formData = new URLSearchParams(body).toString();
+    const { name, email, gdpr, hp, list, subform, boolean } = JSON.parse(event.body);
 
-    // Make an AJAX request using the Fetch API to the target API
-    const response = await fetch("https://postal.hackclub.com/subscribe", {
-      method: "POST",
-      body: JSON.stringify(formData),
+    // Make the cURL request using axios
+    const response = await axios.post('https://postal.hackclub.com/subscribe', {
+      name,
+      email,
+      gdpr,
+      hp,
+      list,
+      subform,
+      boolean,
     });
 
-    // Parse the response from the target API
-    const data = await response.json();
+    // log the response for debugging purposes
+    console.log(response.data);
 
-    // Return a response to the client
-    console.log("data:", data);
+    // Return the response from the external API
     return {
       statusCode: response.status,
-      body: JSON.stringify({ message: data.message }),
+      body: JSON.stringify(response.data),
     };
   } catch (error) {
-    console.error("Error:", error);
-
-    // Return an error response to the client
+    // Return an error response if something goes wrong
+    // log the error for debugging purposes
+    console.log(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Internal Server Error" }),
+      body: JSON.stringify({ error: 'Internal Server Error' }),
     };
   }
 };
