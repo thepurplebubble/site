9cfc8ad1b6fa81b632ec59e4abd548b5e470f391
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-31 21:19:06 -0400

    chore: run prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/branding": "/brand"
+    "/branding": "/brand",
   },
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
