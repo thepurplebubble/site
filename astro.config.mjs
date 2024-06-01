624680fc78e001c78086be28ade28c4af11969cd
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-31 21:16:33 -0400

    chore: Update Astro config for server output and Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/branding": "/brand",
+    "/branding": "/brand"
   },
+  output: "server",
+  adapter: netlify()
 });
